b7432037e811c6cba329c36fab9fb6a12bd79999
brady-t <brady.trieu@gmail.com>
2021-07-02 16:07:57 -0400

    Broke down code to more functions

--- ScrapeIndeed.py
+++ ScrapeIndeed.py
@@ -3,9 +3,51 @@ from bs4 import BeautifulSoup
 import mysql.connector
 import re
 
+def setUpDB(myHost,myUser,myPassword,myDB):
+    return mysql.connector.connect(
+                host=myHost, 
+                user=myUser,
+                passwd=myPassword,
+                database = myDB
+                )
+
+def saveToDB(database, query,values):
+    mycursor = database.cursor()
+    mycursor.execute(query,values)
+    database.commit()
+    print("Successfully saved info, ratings to database")
+
+def findEmployeeStatus(text):
+    pattern1 = re.compile(r'\(((Current Employee)|(Former Employee))\)')
+    status = (pattern1.search(text))
+    startIndexStatusParenthesis = status.span()[0]
+    endIndexStatusParenthesis = status.span()[1]
+    return (text[startIndexStatusParenthesis:endIndexStatusParenthesis]).lstrip("(").rstrip(")")
+
+def findTitle(text):
+    indexParenthesis = text.find("(")
+    pattern1 = re.compile(r'\(((Current Employee)|(Former Employee))\)')
+    status = (pattern1.search(text))
+    startIndexStatusParenthesis = status.span()[0]
+    return text[:startIndexStatusParenthesis]
+
+def findLocation(text):
+    listDashIndex = []
+    for i in re.finditer('-',text):
+        listDashIndex.append(i.span()[1])
+    location = text[listDashIndex[0]:listDashIndex[1]]
+    location = location.rstrip("-")
+    return location.strip()
+
+def findDate(text):
+    listDashIndex = []
+    for i in re.finditer('-',text):
+        listDashIndex.append(i.span()[1])   
+    return (text[listDashIndex[1]:]).strip()
+    
 def scrape(URL):
-    # set up MySQL 
-        
+    # set up MySQL     
+    
     db = setUpDB("localhost","root","root","testdatabase")
 
     while True:
@@ -26,12 +68,10 @@ def scrape(URL):
         ratings = soup.find_all("button",class_= "css-1hmmasr-Text e1wnkr790")
 
         # find review-id
+
         ids = soup.find_all("div", attrs={"data-tn-entitytype" : "reviewId"}, class_="css-snxk27-Flex e37uo190")
-        
         idList = []
-        
         for id in ids:
-            
             idList.append(id['data-tn-entityid'])
             
         # find info of reviews 
@@ -45,29 +85,19 @@ def scrape(URL):
 
             # status
 
-            pattern1 = re.compile(r'\(((Current Employee)|(Former Employee))\)')
-            status = (pattern1.search(text))
-            startIndexStatusParenthesis = status.span()[0]
-            endIndexStatusParenthesis = status.span()[1]
-            employeeStatus = (text[startIndexStatusParenthesis:endIndexStatusParenthesis]).lstrip("(").rstrip(")")
+            employeeStatus = findEmployeeStatus(text)
 
             # title
 
-            indexParenthesis = text.find("(")
-            title = text[:startIndexStatusParenthesis]
+            title = findTitle(text)
             
             # location
 
-            listDashIndex = []
-            for i in re.finditer('-',text):
-                listDashIndex.append(i.span()[1])
-            location = text[listDashIndex[0]:listDashIndex[1]]
-            location = location.rstrip("-")
-            location = location.strip()
+            location = findLocation(text)
 
             # date of publication
 
-            date = (text[listDashIndex[1]:]).strip()
+            date = findDate(text)
 
             # save to DB
 
@@ -77,25 +107,12 @@ def scrape(URL):
             saveToDB(db,query,values)
            
         # scrape next page of reviews if it exists
-        
+
         link = soup.find("a",attrs={"data-tn-element" : "next-page", "data-tn-link" : "true"})
         if link == None or len(link) == 0:
             break
         else:
             URL = "https://www.indeed.com" + (link.get('href'))
 
-def setUpDB(myHost,myUser,myPassword,myDB):
-    return mysql.connector.connect(
-                host=myHost, 
-                user=myUser,
-                passwd=myPassword,
-                database = myDB
-                )
-
-def saveToDB(database, query,values):
-    mycursor = database.cursor()
-    mycursor.execute(query,values)
-    database.commit()
-    print("Successfully saved info, ratings to database")
 
 scrape("https://www.indeed.com/cmp/Systems-Planning-and-Analysis,-Inc.-(spa)/reviews?fcountry=ALL&start=0")
